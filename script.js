d745e74272df9b73725d494fe4c5ed57d3d3e8a8
Abderrahim Ajakka <ajakka.abderrahim@gmail.com>
2024-10-13 13:55:49 +0100

    script.js doesn't contain function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         // Retrieve and trim inputs
